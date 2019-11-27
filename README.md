ecaaee89ce87450cd5d2f5b8fd85564d67eaddf1
Gustavo <guslopezcallejas@gmail.com>
2019-11-27 14:49:11 -0600

    Correccion

---
+++ README.md
@@ -0,0 +1,5 @@
+# MusicBot
+This is a MusicBot made with Node.js and Last.fm   
+Watch this Video for more: https://www.youtube.com/watch?v=mDPaqujfXKA      
+     
+If you want to try: https://www.facebook.com/MusicBot-128415561249659
--- index.js
+++ index.js
@@ -1,48 +1,205 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var path = require('path');
+const LastFM = require('last-fm')
+const lastfm = new LastFM('////YOUR LAST_FM API KEY/////', {
+    userAgent: 'DemoApp/1.0.0 (////DOMAIN NAME OF THE SERVER////)'
+})
 
-const app = express().use(bodyParser.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-app.post('/webhook', (req, res) =>{
-    console.log('POST: webhook');
+let FACEBOOK_VERIFY_TOKEN = "/////UNIQUE SECRET KEY/////";
+let FACEBOOK_PAGE_ACCESS_TOKEN = "EAAIwO6sfsoMBAOeWQ0QkDxpZAZBx6HPRrKjFLtpfCIhZCcO0CCzdNgSFb0J9AkChF5ZATM1EsrEwHZCCVYVbPYMhrty2SKLEZCfeOPIXSOp3QPwrjoaFIEnxtG8Ol5WY1cxZBJtD2iLPIcZAW3G8bK8noYJkkZAnZA9dWLIaKbSF1OIuq6fDJLJZCaeQ5WS8eAApdYZD";
+let FACEBOOK_SEND_MESSAGE_URL = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + FACEBOOK_PAGE_ACCESS_TOKEN;
+let LAST_FM_URL = 'https://www.last.fm/music/';
 
-    const body = req.body;
+app.set('port', (process.env.PORT || 5000));
 
-    if(body.object === 'page'){
-    
-        body.entry.forEach(entry => {
-            // se reciben y procesan los mensajes
-            const webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
-        });
+app.use(express.static(__dirname + '/public'));
 
-        res.status(200).send('EVENTO RECIBIDO');
-    }else{
-        res.sendStatus(404);
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/webhook/', function(request, response) {
+    if (request.query['hub.verify_token'] == FACEBOOK_VERIFY_TOKEN) {
+        response.send(request.query['hub.challenge'])
     }
+    res.send('Error, wrong token')
 });
 
-app.get('/webhook', (req, res) =>{
-    console.log('GET: webhook');
+app.get('/privacy', function(req, res) {
+    res.sendFile(path.join(__dirname, 'views', 'pages', 'privacy.html'));
+});
 
-    const VERIFY_TOKEN = 'stringUnicoParaTuAplicacion';
+app.post('/webhook', function(req, res) {
+    if (req.body.object === 'page') {
+        if (req.body.entry) {
+            req.body.entry.forEach(function(entry) {
+                if (entry.messaging) {
+                    entry.messaging.forEach(function(messagingObject) {
+                        var senderId = messagingObject.sender.id;
+                        if (messagingObject.message) {
+                            var musicName = messagingObject.message.text;
+                            fetchData(senderId, musicName);
+                        } else if (messagingObject.postback) {
+                            console.log('Recieved postback');
 
-    const mode = req.query['hub.mode'];
-    const token = req.query['hub.verify_token'];
-    const challenge = req.query['hub.challenge'];
-
-    if(mode && token){
-        if(mode === 'subscribe' && token === VERIFY_TOKEN){
-            console.log('WEBHOOK VERIFICADO');
-            res.status(200).send(challenge);
-        }else{
-            res.sendStatus(404);
+                        }
+                    });
+                } else {
+                    console.log('no message key found');
+                }
+            });
+        } else {
+            console.log('no entry key found');
         }
-    }else{
-        res.sendStatus(404);
+    } else {
+
     }
+    res.status(200).send();
 });
 
-app.listen(3000, () =>{
-    console.log('Servidor iniciado...');
+function sendMessageToUser(senderId, message) {
+    request({
+        url: FACEBOOK_SEND_MESSAGE_URL,
+        method: 'POST',
+        json: {
+            "recipient": {
+                "id": senderId
+            },
+            "message": {
+                "text": message
+            }
+        }
+
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending UIMESSAGE to User ' + JSON.stringify(error));
+        } else if (response.body.error) {
+            console.log('Error sending UImessage' + JSON.stringify(response.body.error));
+        }
+    });
+}
+
+function fetchData(senderId, musicName) {
+    showTypingIndicatorToUser(senderId, true);
+    var opts = {
+        q: musicName
+    };
+    lastfm.trackSearch(opts, (err, data) => {
+        showTypingIndicatorToUser(senderId, false);
+        if (err) {
+            console.error(err)
+        } else {
+            fetchingData(senderId, data);
+        }
+    });
+}
+
+function fetchingData(senderId, body) {
+    var elements = [];
+    if (body.result) {
+        if (body.result.length > 0) {
+            console.log('under result');
+            var lengthOfResult = body.result.length > 10 ? 10 : body.result.length;
+            for (i = 0; i < lengthOfResult; i++) {
+                elements.push(formingElements(body.result[i]));
+            }
+            sendTemplateResponse(senderId, elements);
+        } else {
+            sendMessageToUser(senderId, 'Couldn\'t find info');
+        }
+
+    }
+}
+
+function formingElements(result) {
+    var musicName = result.name;
+    var artistName = result.artistName;
+    var posterPath;
+
+    if (result.images.length == 0 || result.images == undefined) {
+        posterPath = 'https://images.pexels.com/photos/3104/black-and-white-music-headphones-life.jpg?h=350&auto=compress&cs=tinysrgb';
+    } else {
+        posterPath = result.images[2];
+    }
+
+    var musicNameArray = musicName.split(' ');
+    var artistNameArray = artistName.split(' ');
+    var musicNameUrl = musicNameArray.join('+');
+    var artistNameUrl = artistNameArray.join('+');
+
+    return {
+        title: musicName,
+        subtitle: artistName,
+        image_url: posterPath,
+        buttons: [{
+            "type": "web_url",
+            "url": LAST_FM_URL + artistNameUrl + '/_/' + musicNameUrl,
+            "title": "More Details"
+        }]
+    }
+}
+
+function sendTemplateResponse(senderId, elementList) {
+    request({
+        url: FACEBOOK_SEND_MESSAGE_URL,
+        method: 'POST',
+        json: {
+            recipient: {
+                id: senderId
+            },
+            message: {
+                attachment: {
+                    type: 'template',
+                    payload: {
+                        template_type: 'generic',
+                        elements: elementList
+
+
+                    }
+                }
+            }
+
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending UIMESSAGE to User ' + error.toString());
+        } else if (response.body.error) {
+            console.log('Error sending UImessage under sendTemplateResponse' + JSON.stringify(response.body.error));
+        }
+        //ignore
+    });
+}
+
+function showTypingIndicatorToUser(senderId, isTyping) {
+    var senderAction = isTyping ? 'typing_on' : 'typing_off';
+    request({
+        url: FACEBOOK_SEND_MESSAGE_URL,
+        method: 'POST',
+        json: {
+            recipient: {
+                id: senderId
+            },
+            sender_action: senderAction
+        }
+
+    }, function(error, response, body) {
+        if (error) {
+            console.log('sending Typing indicator to user ' + error);
+        } else if (response.body.error) {
+            console.log('Error sending typing indicator' + response.body.error);
+        }
+    });
+
+}
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });
