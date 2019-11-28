f7eea8dbfca27591e0ca378bb835f2bf5c3fa3fe
Gustavo <guslopezcallejas@gmail.com>
2019-11-27 23:35:11 -0600

    2

--- server.js
+++ server.js
@@ -1,11 +1,54 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+
+const app = express().use(bodyParser.json());
+
 const path = require('path');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 4000, function(){
+app.post('/webhook', (req, res) => {
+    console.log('POST: webhook');
+
+    const body = req.body;
+
+    if (body.object === 'page') {
+
+        body.entry.forEach(entry => {
+            // se reciben y procesan los mensajes
+            const webhookEvent = entry.messaging[0];
+            console.log(webhookEvent);
+        });
+
+        res.status(200).send('EVENTO RECIBIDO');
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.get('/webhook', (req, res) => {
+    console.log('GET: webhook');
+
+    const VERIFY_TOKEN = 'stringUnicoParaTuAplicacion';
+
+    const mode = req.query['hub.mode'];
+    const token = req.query['hub.verify_token'];
+    const challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK VERIFICADO');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(404);
+        }
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+app.listen(process.env.PORT || 4000, function() {
     console.log('Your node js server is running');
 });
