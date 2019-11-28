6daf336407c83eb60b1e773d9c516ae2d3f27dcd
Gustavo <guslopezcallejas@gmail.com>
2019-11-28 09:13:15 -0600

    Mensaje

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const request = require('request');
+
 
 const app = express().use(bodyParser.json());
-
 const path = require('path');
+
+
+
+const PAGE_ACESS_TOKEN = process.env.PAGE_ACESS_TOKEN;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -20,6 +25,17 @@ app.post('/webhook', (req, res) => {
             // se reciben y procesan los mensajes
             const webhookEvent = entry.messaging[0];
             console.log(webhookEvent);
+
+            const sender_psid = webhookEvent.sender.id;
+            console.log('Sender PSID: ${sender_psid}');
+
+            // validar que estamos recibiendo un mensaje
+
+            if(webhookEvent.mensaje) {
+                handleMessage(sender_psid,webhookEvent,message);
+            }else if(webhook.postback ){
+                handlePostback(sender_psid,webhookEvent.postback)
+            }
         });
 
         res.status(200).send('EVENTO RECIBIDO');
@@ -49,6 +65,44 @@ app.get('/webhook', (req, res) => {
     }
 });
 
+function handleMessage(sender_psid, received_message){
+    let response;
+
+    if(received_message.text){
+        response = {
+            'text': 'Tu mensaje fue ${received_message.text}'
+        }
+    }//end if
+
+    callSendAPI(sender_psid, response);
+}
+
+function handlePostback(sender_psid, received_postback){
+    
+}
+function callSendAPI(sender_psid, response){
+
+    const requestBody = {
+         'recipient': {
+            'id' : sender_psid
+         },
+         'message' : response 
+    };
+
+    request({
+        'uri':'https://graph.facebook.com/v2.6/me/messages',
+        'qs':{'access_token': PAGE_ACESS_TOKEN },
+        'method': 'POST'.
+        'json':requestBody
+    }, (err,res, body) => {
+        if(!err){
+            console.log('Mensaje enviado de vuelta')
+        }else{
+            console.log('No se pudo enviar')
+        }
+    } )
+    
+}
 app.listen(process.env.PORT || 4000, function() {
     console.log('Your node js server is running');
 });
